3aa79ef64b879281cfc58452846c836f1e06bfc2
root <root@DESKTOP-JS1CHDV>
2024-05-04 15:52:51 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.Common;
 using System.IO;
 using System.IO.Compression;
 using System.Security.AccessControl;
@@ -76,6 +77,8 @@ else if (command == "cat-file")
 }
 else if (command == "hash-object")
 {
+    //hash object
+
     string modifier = args[1];
     string fileName = args[2];
     if (modifier is not "-w")
@@ -83,20 +86,72 @@ else if (command == "hash-object")
         throw new ArgumentException($"Unknown modifier {modifier}");
     }
 
+    //read file provided in command
     var file = File.ReadAllBytes(fileName);
 
     //before hash I have to add blob header
     byte[] bufferArray = CreateBlob(file);
-    //not get hash form blob content
+    //get hash checksum form blob content
     string hash = HashSHA1(bufferArray).ToLower();
     Console.Write(hash);
 
     //compress before write file into object
     byte[] compressedBlob = Compress(bufferArray);
 
+    //Save compressed file inside Git database
     string path = Path.Combine(".git", "objects", hash.Substring(0, 2), hash.Substring(2));
     Directory.CreateDirectory(Path.Combine(".git", "objects", hash.Substring(0, 2)));
     File.WriteAllBytes(path, compressedBlob);
+}
+else if (command == "ls-tree")
+{
+    var modifier = args[1];
+    var hash = args[2];
+
+    string path = Path.Combine(".git", "objects", hash.Substring(0, 2), hash.Substring(2));
+    using FileStream fs = new FileStream(path, FileMode.Open);
+    byte[] decompress;
+    //decompress tree
+    using MemoryStream ms = new MemoryStream();
+    using (ZLibStream zs = new ZLibStream(fs, CompressionMode.Decompress))
+    {
+        zs.CopyTo(ms);
+        decompress = ms.ToArray();
+    }
+
+    //we have to parse content
+    // tree <size>\x00
+    //     <mode> <name>\x00<20_byte_sha>
+    //     <mode> <name>\0<20_byte_sha>
+    List<(string mode, string fileName, byte[] sha1)> entries = new List<(string mode, string fileName, byte[] sha1)>();
+
+    int index = 0;
+    //skip tree header
+    while (index < decompress.Length && decompress[index] != 0) index++;
+    index++;
+
+    while (index < decompress.Length)
+    {
+        //mode
+        int spaceIndex = Array.IndexOf(decompress, (byte)' ', index);
+        if (spaceIndex == -1) break;
+        string mode = Encoding.ASCII.GetString(decompress, index, spaceIndex - index);
+        index = spaceIndex + 1;
+
+        //filename
+        int nullIndex = Array.IndexOf(decompress, (byte)0, index);
+        if (nullIndex == -1) break;
+        string fileName = Encoding.ASCII.GetString(decompress, index, nullIndex - index);
+        index = nullIndex + 1;
+
+        //sha hash (20 bytes)
+        byte[] sha1 = new byte[20];
+        Array.Copy(decompress, index, sha1, 0, 20);
+        index += 20;
+
+        entries.Add((mode, fileName, sha1));
+        Console.WriteLine(fileName);
+    }
 }
 else
 {
@@ -104,6 +159,8 @@ else
 }
 static byte[] CreateBlob(byte[] file)
 {
+    //blob object in git has special header.
+    //we have to add it before real content
     string header = $"blob {file.Length}\0";
     byte[] headerBytes = Encoding.UTF8.GetBytes(header);
     List<byte> buffer = new List<byte>();
@@ -111,8 +168,20 @@ static byte[] CreateBlob(byte[] file)
     buffer.AddRange(file);
     return buffer.ToArray();
 }
+
+static byte[] Decompress(byte[] buffer)
+{
+    using MemoryStream ms = new MemoryStream();
+    using (ZLibStream zLibStream = new ZLibStream(ms, CompressionMode.Decompress))
+    {
+        zLibStream.Read(buffer, 0, buffer.Length);
+        return ms.ToArray();
+    }
+}
+
 static byte[] Compress(byte[] input)
 {
+    //Git use Zlib compression
     using MemoryStream ms = new MemoryStream();
     using (ZLibStream zLibStream = new ZLibStream(ms, CompressionLevel.Optimal))
     {
@@ -123,5 +192,6 @@ static byte[] Compress(byte[] input)
 
 static string HashSHA1(byte[] input)
 {
+    //Git use SHA1 algorithm to generate hash checksum
     return Convert.ToHexString(SHA1.HashData(input));
 }
